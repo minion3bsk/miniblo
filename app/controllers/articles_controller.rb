1b3b5d3b60338f7b80861f1acf52e3fc22879788
EC2 Default User <ec2-user@ip-172-31-40-40.ap-northeast-1.compute.internal>
2020-05-20 05:32:39 +0000

    投稿機能の完成

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,10 +6,12 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end  
   
-
+  def create
+    Article.create(article_params)
+  end    
   
   private
   def article_params
-    params.permit(:text,:title)
+    params.require(:article).permit(:text,:title)
   end  
 end
