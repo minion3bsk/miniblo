57f6f1630ebe7e7c078b7747c761e32d02c6a6a8
EC2 Default User <ec2-user@ip-172-31-40-40.ap-northeast-1.compute.internal>
2020-05-22 11:27:27 +0000

    minimagick導入のために情報を全消去し、バリデーションとdevise許可まで追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,10 @@
 class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+  
+  def show
+    @user = User.find(params[:id]) if params[:id]
+    @articles = @user.articles
+  end  
 end
