174c846803735d9c3017a3f41167f82caf34617d
EC2 Default User <ec2-user@ip-172-31-40-40.ap-northeast-1.compute.internal>
2020-05-23 02:49:43 +0000

    ymg導入前のセーブ

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -2,12 +2,6 @@ class ImageUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
    include CarrierWave::MiniMagick
-   
-    process resize_to_fit: [800, 800]
-
-    version :thumb do
-      process resize_to_fill: [200,200]
-    end
 
 
   # Choose what kind of storage to use for this uploader:
@@ -42,13 +36,14 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
-  # def extension_whitelist
-  #   %w(jpg jpeg gif png)
-  # end
+   def extension_whitelist
+     %w(jpg jpeg gif png)
+   end
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
   # def filename
   #   "something.jpg" if original_filename
   # end
+  #=> `convert input.jpg -resize 100x100 -negate output.jpg`
 end
