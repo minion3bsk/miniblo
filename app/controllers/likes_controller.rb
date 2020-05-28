6f08030da320877e5d9c4142ab4f4ca295cab9ad
EC2 Default User <ec2-user@ip-172-31-40-40.ap-northeast-1.compute.internal>
2020-05-28 04:34:22 +0000

    いいねajax完成

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,13 +1,13 @@
 class LikesController < ApplicationController
   def create
     @like = Like.create(user_id: current_user.id, article_id: params[:article_id])
+    @article = Article.find(params[:article_id])
   end
 
   def destroy
     like = Like.find_by(user_id: current_user.id, article_id: params[:id])
     like.destroy
     @likes = Like.where(article_id: params[:id])
-    @articles = Article.all
-    redirect_to root_path
+    @article = Article.find(params[:article_id])
   end
 end
